05639c7cd294c2cc5787e9e1e5f4b67784b6aaf4
Jakub Slawecki <jakubslawecki@gmail.com>
2018-07-16 16:50:07 +0200

    First steps in ARKit

--- followTheWorld.KindOf/ViewController.swift
+++ followTheWorld.KindOf/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()  // node is simply possition in space, it's not visible by itself
+        node.geometry = SCNBox(width: 0.15, height: 0.15, length: 0.15, chamferRadius: 0.02)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white // reflecting in white collor
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.darkGray
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(x, y, z)
+        self.sceneView.scene.rootNode.addChildNode(node)
     }
-
+    
+    @IBAction func reset(_ sender: Any) {
+        self.restartTheSession()
+    }
+    
+    func restartTheSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+    
 
 }
 
